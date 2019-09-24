ed3ca7d5beada71fc3724985931e9fa7afec95b6
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:31:05 +0530

    Add files via upload

---
+++ C#/Concepts/Static/Static Constructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj1 = new Demo();
+            Demo obj2 = new Demo();
+        }
+    }
+
+    class Demo
+    {
+        public static int i;
+
+        public int j;
+
+        public Demo()
+        {
+            Console.WriteLine("Inside Constructor..");
+            i = 0;
+            j = 0;
+        }
+
+        static Demo()
+        {
+            i = 0;
+
+            //j = 0;
+        }
+    }
+}
