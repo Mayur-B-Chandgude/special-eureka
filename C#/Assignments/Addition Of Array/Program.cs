15dc9ce45bd944d5894078cdc7e7fe3ff25fd46f
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 22:21:46 +0530

    Add files via upload

---
+++ C#/Assignments/Addition Of Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AdditionOfArray
+{
+    class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of Elements:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] Arr = new int[size];
+
+            Console.WriteLine("Enter Elements in Array:");
+            for (int i = 0; i < size; i++)
+            {
+                Arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Addition obj = new Addition();
+
+            int ret = obj.Add(Arr , size);
+            Console.WriteLine("Addition is:{0}", ret);
+        }
+    }
+
+    class Addition
+    {
+        public unsafe int Add(int [] Arr , int size)
+        {
+            int sum = 0;
+
+            for(int i = 0 ; i < size ; i++)
+            {
+                sum = sum + Arr[i];
+            }
+            return sum;
+        }
+    }
+}
