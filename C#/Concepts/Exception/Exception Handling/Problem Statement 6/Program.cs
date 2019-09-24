f21406aad0800e433af28b45eee466e5f23188ab
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:18:54 +0530

    Add files via upload

---
+++ C#/Concepts/Exception/Exception Handling/Problem Statement 6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj = new Demo();
+
+            obj.fun();
+        }
+    }
+
+    class Demo
+    {
+        public int No1, No2;
+
+        public Demo()
+        {
+            Console.WriteLine("Enter First Number:");
+            No1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            No2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void fun()
+        {
+            try
+            {
+                int ans = No1 / No2;
+                Console.WriteLine("Hello");
+            }
+
+           // Console.WriteLine("hello");
+
+          /* finally
+            {
+                Console.WriteLine("Inside Finally Block..");
+            }*/
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Inside 2 Catch Block..");
+                Console.WriteLine(e);
+            }
+           
+            Console.WriteLine("Rest of the code..");
+        }
+    }
+}
