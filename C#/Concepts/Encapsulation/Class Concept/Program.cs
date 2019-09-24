b2d53efde2c194791d5f4bee5105acc111c65061
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:10:40 +0530

    Add files via upload

---
+++ C#/Concepts/Encapsulation/Class Concept/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee eobj1 = new Employee();
+            Employee eobj2 = new Employee();
+
+            eobj1.Accept();
+            eobj1.Display();
+        }
+    }
+
+    class Employee
+    {
+        public int Eid;
+        public string name;
+        public int Esalary;
+
+        public void Accept()
+        {
+            Console.Write("Enter Employee ID:");
+            Eid = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter Employee Name:");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Employee Salary:");
+            Esalary = Convert.ToInt16(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Details As:");
+            Console.WriteLine("Employee ID is:{0}", Eid);
+            Console.WriteLine("Employee Name is:{0}", name);
+            Console.WriteLine("Employee Salary is:{0}", Esalary);
+        }
+    }
+}
