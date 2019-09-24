0b679f4a1b9423ee5ca1a5f7c17927f15dd7aced
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:13:53 +0530

    Add files via upload

---
+++ C#/Concepts/Exception/Exception Handling/Problem Statement 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj = new Demo();
+
+            obj.fun(10, 0);
+        }
+    }
+
+    class Demo
+    {
+        public void fun(int No1 , int No2)
+        {
+            int ans = No1 / No2;
+
+            Console.WriteLine("Code Successfully Executed..");
+        }
+    }
+}
