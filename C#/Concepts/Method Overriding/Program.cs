2d35779e76b2c4ced92891c57a455c81ca0e6d3b
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:40:08 +0530

    Add files via upload

---
+++ C#/Concepts/Method Overriding/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base bobj = new Base();
+
+            Derived dobj = new Derived();
+
+            bobj.fun();
+            bobj.gun();
+
+            dobj.fun();
+            dobj.gun();
+
+            Base obj = new Derived();
+
+            obj.fun();
+            obj.gun();
+        }
+    }
+
+    class Base
+    {
+        public void fun()
+        {
+            Console.WriteLine("Inside base fun..");
+        }
+
+        public virtual void gun()
+        {
+            Console.WriteLine("Inside base gun..");
+        }
+    }
+
+    class Derived : Base
+    {
+        public void fun()
+        {
+            Console.WriteLine("Inside derived fun..");
+        }
+
+        public override void gun()
+        {
+            //base.gun();
+            Console.WriteLine("Inside derived gun..");
+        } 
+    }
+}
