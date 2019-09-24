cc899fcde48078734fbdde053c23f8577e959b50
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:09:00 +0530

    Add files via upload

---
+++ C#/Concepts/DLL/Implicit Linking/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Library;
+
+namespace Implict_Linking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Implict linking main class..");
+
+            Class1 obj = new Class1();
+
+            obj.fun();
+        }
+    }
+}
