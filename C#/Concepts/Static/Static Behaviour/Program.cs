59b1dc54492184fe66726f15c674fccd457758e2
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:24:20 +0530

    Add files via upload

---
+++ C#/Concepts/Static/Static Behaviour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Behaviour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticDemo.gun();
+
+            StaticDemo obj = new StaticDemo();
+
+            obj.fun();
+        }
+    }
+
+    class StaticDemo
+    {
+        public int i;
+        public int j;
+
+        public static int k;
+        public static int m = 11;
+
+        public void fun()
+        {
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+        }
+
+        public static void gun()
+        {
+           // Console.WriteLine(i);
+           // Console.WriteLine(j);
+
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+        }
+
+        public StaticDemo()
+        {
+            i = 21;
+            j = 51;
+        }
+    }
+}
