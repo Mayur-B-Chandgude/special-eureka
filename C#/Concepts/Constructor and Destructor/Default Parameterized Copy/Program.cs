0715c79e070fc19fe283a46ff820208b9c108b18
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:02:05 +0530

    Add files via upload

---
+++ C#/Concepts/Constructor and Destructor/Default Parameterized Copy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_Parameterized_Copy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj1 = new Demo();
+
+            Console.Write("Enter Number:");
+            int value = Convert.ToInt16(Console.ReadLine());
+
+            Demo obj2 = new Demo(value);
+
+            Demo obj3 = new Demo(obj2);
+        }
+    }
+
+    class Demo
+    {
+        public int i;
+
+        public Demo()
+        {
+            this.i = 11;
+            Console.WriteLine("Inside Default Constructor..");
+        }
+
+        public Demo(int No)
+        {
+            this.i = No;
+            Console.WriteLine("Inside Parametrized Constructor..");
+        }
+
+        public Demo(Demo obj)
+        {
+            this.i = obj.i;
+            Console.WriteLine("Inside Copy Constructor..");
+        }
+
+        ~Demo()
+        {
+            Console.WriteLine("Inside Destructor..");
+        }
+    }
+}
