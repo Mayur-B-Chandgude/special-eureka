790370307051e115e0267aace848339e108c07e2
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 10:25:24 +0530

    Add files via upload

---
+++ Java/Assignments/Count of Characters in String/Demo.java
@@ -0,0 +1,44 @@
+import java.lang.*;
+import java.util.*;
+
+public class Demo
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter String:");
+		String str = scan.nextLine();
+
+		System.out.println("Enter Character:");
+		char chvalue = scan.next().charAt(0);
+
+		Display obj = new Display();
+
+		int ret = obj.Fun(str , chvalue);
+		System.out.println("Count is:" + ret);
+	}
+}
+
+class Display
+{
+	public int Fun(String str , char ch)
+	{
+		int cnt = 0;
+
+		if(str == null)
+		{
+			return 0;
+		}
+
+		for(int i = 0 ; i < str.length() ; i++)
+		{
+			if(str.charAt(i) == ch)
+			{
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+}
+
