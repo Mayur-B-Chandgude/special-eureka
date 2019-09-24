73ef26c9d652fcccd1d7d2d9edbafc00800cd6ef
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 15:18:16 +0530

    Add files via upload

---
+++ Java/Assignments/Annagram/TwoWhile_TwoArray/Demo.java
@@ -0,0 +1,64 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Demo
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter First Number:");
+		int value1 = scan.nextInt();
+
+		System.out.println("Enter Second Number:");
+		int value2 = scan.nextInt();
+
+		Display obj = new Display();
+
+		obj.Fun(value1 , value2);
+	}
+}
+
+class Display
+{
+	public void Fun(int No1 , int No2)
+	{
+		int digit = 0;
+		int i = 0;
+		
+		int Arr[] = new int[10];
+		int Brr[] = new int[10];
+
+		while(No1 != 0)
+		{
+			digit = No1 % 10;
+			Arr[digit]++;
+			No1 = No1 / 10;
+		}
+		while(No2 != 0)
+		{
+			digit = No2 % 10;
+			Brr[digit]++;
+			No2 = No2 / 10;
+		}
+
+		for(i = 0 ; i < 10 ; i++)
+		{
+			if(Arr[i] != Brr[i])
+			{
+				break;
+			}
+		}
+
+		if(i == 10)
+		{
+			System.out.println("Numbers are Annagram");
+		}
+		else
+		{
+			System.out.println("Numbers are not Annagram");
+		}
+	}
+}
+
