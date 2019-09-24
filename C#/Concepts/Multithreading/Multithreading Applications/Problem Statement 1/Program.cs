9879253001f395d76cd6454b0be1d47c0e0b2ecb
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:42:09 +0530

    Add files via upload

---
+++ C#/Concepts/Multithreading/Multithreading Applications/Problem Statement 1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//Multithreaded application with Static Thread procedure.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiple thread as a static method performing same task..");
+
+            Thread t1 = new Thread(new ThreadStart(SingleTask1.DisplayF));
+            Thread t2 = new Thread(new ThreadStart(SingleTask1.DisplayF));
+
+            t1.Name = "Marvellous First";
+            t2.Name = "Marvellous Second";
+
+            t1.Start();
+            t2.Start();
+        }
+    }
+
+    public class SingleTask1
+    {
+        public static void DisplayF()
+        {
+            Thread t = Thread.CurrentThread;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(t.Name + " is running ");
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
