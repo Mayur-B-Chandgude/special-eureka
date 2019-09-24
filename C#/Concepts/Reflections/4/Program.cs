117f0f98aada69faa7aa09830103e9a7d4f0bea0
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:20:01 +0530

    Add files via upload

---
+++ C#/Concepts/Reflections/4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(string);
+
+            Console.WriteLine("Fields of {0} type..", t);
+
+            FieldInfo[] FI = t.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (FieldInfo f in FI)
+            {
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
