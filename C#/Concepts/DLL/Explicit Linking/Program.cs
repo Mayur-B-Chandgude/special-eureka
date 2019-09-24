eeb90db0ecb6080188ee525bb87ef4f5192d8314
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:07:40 +0530

    Add files via upload

---
+++ C#/Concepts/DLL/Explicit Linking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Explicit_Linking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Application..");
+
+            try
+            {
+                Assembly DLL = Assembly.LoadFile(@"E:\C Sharp\Demo Assingment Program\DLL\Explicit Linking\Library\Library\Marvellous.dll");
+                Console.WriteLine("DLL File Loaded into memory");
+
+                Type[] types = DLL.GetTypes();
+                Console.WriteLine("DataTypes from DLL gets fetched..");
+
+                foreach (Type typ in types)
+                {
+                    Console.WriteLine("DataTypes from DLL is" + typ);
+
+                    object obj = Activator.CreateInstance(typ);
+                    Console.WriteLine("Object gets created of class" + typ);
+
+                    MethodInfo MI = typ.GetMethod("fun");
+                    Console.WriteLine("Address of method fun gets fetced..");
+
+                    MI.Invoke(obj, null);
+
+                    Console.WriteLine("method call complete..");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
