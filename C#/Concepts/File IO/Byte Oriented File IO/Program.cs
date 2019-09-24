4b679ea37af1cbc4eb58ac3e0a2fe22d3272700d
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:25:54 +0530

    Add files via upload

---
+++ C#/Concepts/File IO/Byte Oriented File IO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Byte_Oriented_File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream F = new FileStream("Marvellous.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            for (int i = 0; i <= 50; i++)
+            {
+                F.WriteByte((byte)i);
+            }
+
+            F.Position = 0;
+
+            Console.WriteLine("Contents from file are:");
+
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.Write(F.ReadByte() + " ");
+            }
+
+            F.Close();
+        }
+    }
+}
