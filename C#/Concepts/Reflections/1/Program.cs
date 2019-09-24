ff7c85d4c64cb272c4cbe0cb0f61faf2eb85f351
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:15:51 +0530

    Add files via upload

---
+++ C#/Concepts/Reflections/1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t2 = typeof(Marvellous);
+            Console.WriteLine(t2.Assembly + "\n");
+
+            Type t1 = typeof(string);
+            Console.WriteLine(t1.Assembly + "\n");
+        }
+    }
+
+    class Marvellous
+    {
+        public int No1;
+        public int No2;
+
+        public void fun()
+        {
+            //logic
+        }
+    }
+}
