32384b344432d773b03a9dd8bbcc5a444affce27
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:23:28 +0530

    Add files via upload

---
+++ C#/Concepts/Exception/User Defined Exception/Problem Statement Checked Unchecked/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_Checked_Unchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Marvellous.Demo(int.MaxValue);
+
+            //Marvellous.CheckedDemo(int.MaxValue);
+
+            Marvellous.UncheckedDemo(int.MaxValue);
+        }
+    }
+
+    class Marvellous
+    {
+        public static void Demo(int No)
+        {
+           Console.WriteLine("Demo:" + (No + 10));
+        }
+
+        public static void CheckedDemo(int No)
+        {
+            checked
+            {
+                Console.WriteLine("Checked:" + (No + 10));
+            }
+        }
+
+        public static void UncheckedDemo(int No)
+        {
+            unchecked
+            {
+                Console.WriteLine("Unchecked:" + (No + 10));
+            }
+        }
+    }
+}
