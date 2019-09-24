1c16a1d6556bb5e402e9ba0e63be68a466bfe376
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:30:02 +0530

    Add files via upload

---
+++ C#/Concepts/File IO/String Oriented File IO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace String_Oriented_File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Batches = new string[] { "PPA", "LB", "IPD", "LSP", "AWB", "C#", "Python with Automation" };
+
+            using (StreamWriter sw = new StreamWriter("MarvellousBatches.txt"))
+            {
+                foreach (string s in Batches)
+                {
+                    sw.Write(s + "\t");
+                }
+            }
+
+            string line = "";
+            using (StreamReader sr = new StreamReader("MarvellousBatches.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.Write(line + "\t");
+                }
+            }
+        }
+    }
+}
