1bfc53d2f63b4df8006220dcff82aa91174988e5
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 10:52:47 +0530

    Add files via upload

---
+++ Java/Assignments/Number Odd And Side Numbers Even/Demo.java
@@ -0,0 +1,48 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Demo
+{
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Number of Elements:");
+		int size = scan.nextInt();
+
+		int Arr[] =new int[size];
+
+		System.out.println("Enter Data:");	
+		for(int i = 0 ; i < size ; i++)
+		{
+			Arr[i] = scan.nextInt();
+		}
+
+		Display obj = new Display();
+
+		obj.Fun(Arr , size);
+	}
+}
+
+class Display
+{
+	public void Fun(int Arr[] , int size)
+	{
+		if(size <= 0)
+		{
+			return;
+		}
+
+		int add = 0;
+	
+		for(int i = 1 ; i < size ; i++)		
+		{
+			if(((Arr[i] % 2) != 0) && ((Arr[i-1] % 2) == 0) && ((Arr[i+1] % 2) == 0))
+			{
+				System.out.print("\t"+ Arr[i-1] + Arr[i] + Arr[i+1]);
+			}
+		}
+	}
+}
+
