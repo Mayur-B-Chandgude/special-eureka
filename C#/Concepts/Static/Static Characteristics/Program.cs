4dfb20f2b7591bbdd216b146a795a2b8d4fdd8a6
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:26:12 +0530

    Add files via upload

---
+++ C#/Concepts/Static/Static Characteristics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Charactristics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Static Charactristics before object:{0}", StaticDemo.k);
+
+            StaticDemo obj = new StaticDemo();
+
+           // Console.WriteLine("Static Charateristics:{0}", obj.k);
+
+            Console.WriteLine("Static Characteristics after object:{0}", StaticDemo.m);
+        }
+    }
+
+    class StaticDemo
+    {
+        public int i;
+        public int j;
+
+        public static int k;
+        public static int m = 11;
+
+        public StaticDemo()
+        {
+            i = 21;
+            j = 51;
+        }
+    }
+}
