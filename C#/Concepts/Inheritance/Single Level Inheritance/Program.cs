07fb77dd6c4f825f6dd926d35ae5d36672676784
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:35:28 +0530

    Add files via upload

---
+++ C#/Concepts/Inheritance/Single Level Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Level_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base bobj = new Base();
+
+            Derived dobj = new Derived();
+
+            bobj.fun();
+
+            dobj.fun();
+            dobj.gun();
+        }
+    }
+
+    class Base
+    {
+        public int i;
+
+        public void fun()
+        {
+            Console.WriteLine("Inside Base Fun..");
+        }
+    }
+
+    class Derived : Base
+    {
+        public int j;
+
+        public void gun()
+        {
+            Console.WriteLine("Inside Derived Gun..");
+        }
+    }
+}
