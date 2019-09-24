6129ae8d1004acfa2271a852a4e4d784047bbf23
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:28:50 +0530

    Add files via upload

---
+++ C#/Concepts/Static/Static Class/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demo obj = new Demo();
+
+            Demo.gun();
+        }
+    }
+
+    static class Demo
+    {
+        //public int i;
+
+        public static int j = 11;
+
+        /*public void fun()
+        {
+            Console.WriteLine("Inside Non static fun..");
+        }*/
+
+        /*public Demo()
+        {
+            
+        }*/
+
+        public static void gun()
+        {
+        
+        }
+    }
+}
