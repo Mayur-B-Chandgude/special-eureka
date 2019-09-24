d145806b35b6084c93e89425e0ca398af3295217
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 15:31:00 +0530

    Add files via upload

---
+++ C/Problems_On_Array/Palindrome/Palindrome2.c
@@ -0,0 +1,70 @@
+/*********************************** Using For Loop ********************************************/
+
+#include<stdio.h>
+#include<malloc.h>
+
+#define TRUE	1
+#define FALSE	0
+
+typedef int BOOL;
+
+BOOL Display(int* , int);
+
+int main()
+{
+	int i , size = 0;
+	int *ptr = NULL;
+	BOOL bret;
+
+	printf("Enter Number of Elements:\n");
+	scanf("%d",&size);
+
+	ptr = (int*)malloc(size * sizeof(int));
+	
+	printf("Enter data:\n");
+	for(i = 0 ; i < size ; i++)
+	{
+		scanf("%d",&ptr[i]);
+	}
+
+	bret = Display(ptr , size);
+	if(bret == TRUE)
+	{
+		printf("Array is Palindrome\n");
+	}
+	else
+	{
+		printf("Array is not Palindrome\n");
+	}
+	
+	return 0;
+}
+
+BOOL Display(int *Arr , int size)
+{
+	if((Arr == NULL) || (size <= 0))
+	{
+		return FALSE;
+	}
+
+	int start = 0;	
+	int end = 0;
+
+	for(start = 0 , end = size-1 ; start <= end ; start++ , end--)
+	{
+		if(Arr[start] != Arr[end])
+		{
+			break;
+		}
+	}
+
+	if(start < end)
+	{
+		return FALSE;
+	}
+	else
+	{
+		return TRUE;
+	}
+}
+
