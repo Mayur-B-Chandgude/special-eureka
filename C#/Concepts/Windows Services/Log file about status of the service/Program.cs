37db171e5d9832868212d5adc7c1f9adaaf96230
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 21:56:30 +0530

    Add files via upload

---
+++ C#/Concepts/Windows Services/Log file about status of the service/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log_file_about_status_of_the_service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ C#/Concepts/Windows Services/Log file about status of the service/Service1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+
+namespace Log_file_about_status_of_the_service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MarvellousWriteToFile("Marvellous Service Started..");
+        }
+
+        protected override void OnStop()
+        {
+            MarvellousWriteToFile("Marvellous Service Stoped..");
+        }
+
+        public void MarvellousWriteToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\MarvellousLogs";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\MarvellousLogs\\MarvellousServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/','_') + ".txt";
+
+            if(!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using(StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
