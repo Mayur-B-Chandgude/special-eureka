f4ec9256229908b1b0f7d38e5cfee87b49949878
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:55:19 +0530

    Add files via upload

---
+++ C#/Concepts/Multithreading/Thread Class Property And Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marvellous mobj = new Marvellous();
+
+            Thread t1 = new Thread(new ThreadStart(mobj.fun));
+
+            t1.Name = "Thread Marvellous..";
+
+            t1.Start();
+
+            t1.Join();
+
+            Console.WriteLine("Main Thread is running..");
+        }
+    }
+
+    public class Marvellous
+    {
+        public void fun()
+        {
+            Thread t = Thread.CurrentThread;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(t.Name + "is running");
+
+                Console.WriteLine(i);
+            }
+
+            if (t.IsAlive)
+            {
+                Console.WriteLine("Thread is Alive..");
+            }
+
+            Console.WriteLine("Thread Priority of current Thread is:{0}", t.Priority);
+
+            Console.WriteLine("Thread State of Current Thread:{0}", t.ThreadState);
+
+            Console.WriteLine("Thread ID of current Thread is:{0}", t.ManagedThreadId);
+
+            if (t.IsBackground)
+            {
+                Console.WriteLine("Thread is in Background..");
+            }
+        }
+    }
+}
