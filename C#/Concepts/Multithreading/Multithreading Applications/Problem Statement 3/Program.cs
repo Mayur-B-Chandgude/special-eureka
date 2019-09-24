5f7a4c4a2113aabfe1ca8fdf219f2771bd9296ec
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:45:29 +0530

    Add files via upload

---
+++ C#/Concepts/Multithreading/Multithreading Applications/Problem Statement 3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Problem_Statement_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultipleTask mobj = new MultipleTask();
+
+            Thread t5 = new Thread(new ThreadStart(mobj.DisplayF));
+
+            Thread t6 = new Thread(new ThreadStart(mobj.DisplayB));
+
+            t5.Name = "Marvellous Fifth";
+
+            t6.Name = "Marvellous Sixth";
+
+            t5.Start();
+
+            t6.Start();
+        }
+    }
+
+    public class MultipleTask
+    {
+        public void DisplayF()
+        {
+            Thread t = Thread.CurrentThread;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(t.Name + " is running ");
+
+                Console.WriteLine(i);
+            }
+        }
+
+        public void DisplayB()
+        {
+            Thread t = Thread.CurrentThread;
+
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine(t.Name + " is running ");
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
