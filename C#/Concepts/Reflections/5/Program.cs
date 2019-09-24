072a8c13461b1dfa981b9bb12af736bc3c3726d7
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 20:21:19 +0530

    Add files via upload

---
+++ C#/Concepts/Reflections/5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(string);
+
+            Console.WriteLine("EventInfo of {0} type..", t);
+
+            EventInfo[] EI = t.GetEvents(BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (EventInfo e in EI)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
