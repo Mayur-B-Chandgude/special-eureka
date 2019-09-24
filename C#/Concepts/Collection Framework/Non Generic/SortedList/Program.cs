16368b6f72daa7b756b1ee8a7c89208e01e76aeb
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 18:59:03 +0530

    Add files via upload

---
+++ C#/Concepts/Collection Framework/Non Generic/SortedList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl = new SortedList();
+
+            sl.Add("10", "PPA");
+            sl.Add("11", "Angular");
+            sl.Add("12", "LB");
+            sl.Add("13", "IPD");
+            sl.Add("14", "LSP");
+
+            if (sl.ContainsValue("C#"))
+            {
+                Console.WriteLine("This batch already into List");
+            }
+            else
+            {
+                sl.Add("15", "C#");
+            }
+
+            ICollection keys = sl.Keys;
+
+            Console.WriteLine("Keys:");
+            foreach (String s in keys)
+            {
+                Console.WriteLine(sl[s]);
+            }
+
+            sl.Remove("15");
+
+            Console.WriteLine("After Element Removal Sorted List:");
+            foreach (String s in keys)
+            {
+                Console.WriteLine(sl[s]);
+            }
+        }
+    }
+}
