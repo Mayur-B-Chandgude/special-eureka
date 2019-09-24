928923c5b5515a40aa5d88dc0a85211e5369c394
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 18:55:45 +0530

    Add files via upload

---
+++ C#/Concepts/Collection Framework/Non Generic/HashTable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add("10", "PPA");
+            ht.Add("11", "LB");
+            ht.Add("12", "IPD");
+            ht.Add("13", "LSP");
+            ht.Add("14", "Angular");
+
+            ICollection keys = ht.Keys;
+
+            Console.WriteLine("Elements from Hash Table..");
+            foreach (string s in keys)
+            {
+                Console.WriteLine(s + " ");
+            }
+
+            Console.WriteLine("Elements After Removal..");
+            ht.Remove("11");
+            foreach (string s in keys)
+            {
+                Console.WriteLine(s + " ");
+            }
+        }
+    }
+}
