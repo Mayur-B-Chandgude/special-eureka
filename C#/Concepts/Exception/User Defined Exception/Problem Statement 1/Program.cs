8d59630aaa5d2d54b1ae7148da9a088aef001f47
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-24 19:21:17 +0530

    Add files via upload

---
+++ C#/Concepts/Exception/User Defined Exception/Problem Statement 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeInvalidException obj = new AgeInvalidException();
+        }
+    }
+
+    public class AgeInvalidException : Exception
+    {
+        public AgeInvalidException()
+        {
+            Console.WriteLine("Inside Default Constructor..");
+        }
+
+        public AgeInvalidException(String message)
+            : base(message)
+        {
+            Console.WriteLine("Inside Parameterized Constructor..");
+        }
+
+        public AgeInvalidException(string message, Exception inner)
+            : base(message, inner)
+        {
+            Console.WriteLine("Inside Copy Constructor..");
+        }
+    }
+}
