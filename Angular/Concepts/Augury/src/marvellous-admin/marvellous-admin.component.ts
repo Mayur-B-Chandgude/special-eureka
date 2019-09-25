e6094ef968439351a365d0ad379f188ab2cbf90c
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 20:53:44 +0530

    Add files via upload

---
+++ Angular/Concepts/Augury/src/marvellous-admin/marvellous-admin.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous-admin',
+  templateUrl: './marvellous-admin.component.html'
+})
+export class MarvellousAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
