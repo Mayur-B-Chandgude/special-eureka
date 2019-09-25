83d85b8cc9247d91f00c8761ca3890ddc395029e
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 09:19:13 +0530

    Add files via upload

---
+++ C#/Assignments/Assignment Number 10/Problem Statement 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statement_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Birth Date:");
+                DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+                if (DateTime.Today.Year < birthdate.Year)
+                {
+                    throw new InvalidAgeException("Invalid Age");
+                }
+                else
+                {
+                    //Console.WriteLine("Age is Valid..");
+
+                    Marvellous mobj = new Marvellous();
+                    mobj.Display(birthdate);
+                }
+            }
+            catch (InvalidAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string message) : base(message)
+        {
+           
+        }
+    }
+
+    class Marvellous
+    {
+        public void Display(DateTime birthdate)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthdate.Year;
+
+            Console.WriteLine("Age is:{0}", age);
+        }
+    }
+}
