c8578f3e618704152c8aa994ccb8cacbd8ef2ffd
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 10:55:41 +0530

    Add files via upload

---
+++ Angular/Typescript/If Else/IfElse.js
@@ -0,0 +1,10 @@
+function ChkEvenOdd(No) {
+    if ((No % 2) == 0) {
+        console.log("Number is Even..");
+    }
+    else {
+        console.log("Number is Odd..");
+    }
+}
+var value = 12;
+ChkEvenOdd(value);
---
+++ Angular/Typescript/If Else/IfElse.ts
@@ -0,0 +1,17 @@
+
+function ChkEvenOdd(No:number):void
+{
+	if((No % 2) == 0)
+	{
+		console.log("Number is Even..");
+	}
+	else
+	{
+		console.log("Number is Odd..");
+	}
+}
+
+var value:number = 12;
+
+ChkEvenOdd(value);
+
