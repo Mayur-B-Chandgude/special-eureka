8fa8a8cd755fafab807c53e14d1bb9829f90ebe0
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 10:57:40 +0530

    Add files via upload

---
+++ Angular/Typescript/While Loop/WhileLoop.js
@@ -0,0 +1,7 @@
+var No = 5;
+var i = 0;
+while (i <= No) {
+    console.log(i);
+    //i++;
+    No--;
+}
---
+++ Angular/Typescript/While Loop/WhileLoop.ts
@@ -0,0 +1,10 @@
+
+var No:number = 5;
+var i:number = 0;
+
+while(i <= No)
+{
+	console.log(i);
+	i++;			//0 1 2 3 4 5
+	//No--;			//0 0 0 0 0 0
+}
