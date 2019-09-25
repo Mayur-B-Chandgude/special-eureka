60e7f1d59c3495475d1bbc2d2e5ef3d89d357709
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 11:05:52 +0530

    Add files via upload

---
+++ Angular/Typescript/Type Assertion/TypeAssertion.js
@@ -0,0 +1,7 @@
+/*
+TypeScript allows changing a variable from one type to another. TypeScript refers to this process as Type Assertion.
+Type Assertion is Compile time process.
+*/
+var no = "101"; // no is of type string
+var value = no; //value is now of type number 
+console.log("After Type Assertion : " + value);
