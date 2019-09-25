19565bc90057bcaadefd96dc931f46262634d85f
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 10:44:21 +0530

    Add files via upload

---
+++ Angular/Typescript/tuples/7_tuples.ts
@@ -0,0 +1,38 @@
+/*
+
+Application demonstates conncept of Tuples.
+
+Tuples :
+It represents a heterogeneous collection of values.
+Due to which in tuples we can store diffrent types of data typed elements.
+
+*/
+
+// Create and initialise tuples
+var tuple1 = [10,"Marvellous",3.14];
+
+// Accessing elements of tuples
+console.log(tuple1[0]); 
+console.log(tuple1[1]);
+console.log(tuple1[2]);
+console.log("Length of tuple : "+tuple1.length);
+
+// Insert element into tuples
+tuple1.push("Infosystems");
+console.log(tuple1[3]);
+console.log("Length of tuple after push : "+tuple1.length);
+
+// Remove element from tuple
+tuple1.pop();
+console.log("Length of tuple after pop : "+tuple1.length);
+
+// Update value of tuple
+tuple1[0] = "Pune";
+
+// Traversing tuples
+console.log("Elements of tuples are");
+
+for(var i=0; i<tuple1.length;i++)
+{
+    console.log(tuple1[i]);
+}
