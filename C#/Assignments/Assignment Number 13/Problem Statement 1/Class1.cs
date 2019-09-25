b97cdab6087d3cc9af195f03bedf441724ace9d7
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 09:33:26 +0530

    Add files via upload

---
+++ C#/Assignments/Assignment Number 13/Problem Statement 1/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate int del(int No1, int No2);
+
+namespace MarvellousArithmatic
+{
+    public class Class1
+    {
+        public int Add(int No1, int No2)
+        {
+            int result = 0;
+
+            result = No1 + No2;
+            return result;
+        }
+
+        public int Sub(int No1, int No2)
+        {
+            int result = 0;
+
+            result = No1 - No2;
+            return result;
+        }
+
+        public int Mul(int No1, int No2)
+        {
+            int result = 0;
+
+            result = No1 * No2;
+            return result;
+        }
+
+        public int Div(int No1, int No2)
+        {
+            if (No2 == 0)
+            {
+                return -1;
+            }
+
+            int result = 0;
+
+            result = No1 / No2;
+            return result;
+        }
+    }
+}
---
+++ C#/Assignments/Assignment Number 13/Problem Statement 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarvellousArithmatic;
+
+namespace Problem_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number:");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+
+            Class1 obj = new Class1();
+
+            del dobj = new del(obj.Add);
+
+            dobj += new del(obj.Sub);
+
+            dobj += new del(obj.Mul);
+
+            dobj += new del(obj.Div);
+
+            Console.WriteLine("Division is:{0}", dobj(value1, value2));
+            dobj -= new del(obj.Div);
+
+            Console.WriteLine("Multiplication is:{0}", dobj(value1, value2));
+            dobj -= new del(obj.Mul);
+
+            Console.WriteLine("Substration is:{0}", dobj(value1, value2));
+            dobj -= new del(obj.Sub);
+
+            Console.WriteLine("Addition is:{0}", dobj(value1, value2));
+            dobj -= new del(obj.Add);
+        }
+    }
+}
