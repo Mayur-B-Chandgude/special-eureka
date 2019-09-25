7c1cc949660ba70f047a0578bc823c5e864761df
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 09:41:52 +0530

    Add files via upload

---
+++ C#/Assignments/Assignment Number 15/Problem Statement 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem_Statement_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo di = new DriveInfo(@"C:\");
+
+            DirectoryInfo dirinfo = di.RootDirectory;
+
+            FileInfo[] FileNames = dirinfo.GetFiles("*.*");
+
+            foreach (FileInfo fi in FileNames)
+            {
+                Console.WriteLine("{0}:",fi.Name);
+            }
+        }
+    }
+}
