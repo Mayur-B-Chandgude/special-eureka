a99df60062d12c7f7a9f12caa07ffb289ffa916a
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 10:36:27 +0530

    Add files via upload

---
+++ Angular/Typescript/First Program/1_First_Program.ts
@@ -0,0 +1,8 @@
+/*
+    Application which display string on console
+*/
+
+console.log("Marvellous Infosystems Web Development")
+
+var message = "Marvellous Infosystems Web Development" 
+console.log(message)
