7986fba89d578e442d8aaf9c87e3f73cf9300756
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 08:36:29 +0530

    Add files via upload

---
+++ C#/Assignments/Jagged Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            Console.Write("Enter Number of rows:");
+            int row = Convert.ToInt32(Console.ReadLine());
+
+            int[][] Arr = new int[row][];
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                for (int j = 0; j < Arr[i].Length; j++)
+                {
+                    Arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Jagged obj = new Jagged();
+
+            obj.array(Arr);
+        }
+    }
+
+    class Jagged
+    {
+        public unsafe void array(int[][] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                for (int j = 0; j < Arr[i].Length; j++)
+                {
+                    Console.WriteLine(Arr[i][j] + "");
+                }
+            }
+        }
+    }
+}
