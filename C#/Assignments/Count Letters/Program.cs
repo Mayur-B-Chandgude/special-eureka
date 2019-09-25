68d08e1066a5b2f4e9a439a3250f7a5119074fc0
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 08:29:37 +0530

    Add files via upload

---
+++ C#/Assignments/Count Letters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CountLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count obj = new Count();
+
+            obj.Ccount();
+        }
+    }
+
+    class Count
+    {
+        public void Ccount()
+        {
+            Console.WriteLine("Enter String");
+            string str = Console.ReadLine();
+
+            int CapCnt = 0, SmallCnt = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    CapCnt++;
+                }
+                else if(char.IsLower(str[i]))
+                {
+                    SmallCnt++;
+                }
+            }
+
+            Console.WriteLine("Upper:{0}", CapCnt);
+            Console.WriteLine("Lower:{0}", SmallCnt);
+        }
+    }
+}
