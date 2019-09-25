d225f3a17ed61dca3b67c09c9b91796c05d1593c
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 08:35:23 +0530

    Add files via upload

---
+++ C#/Assignments/Factorial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Nunmber:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Factorial obj = new Factorial();
+
+            int ret = obj.Ops(num);
+            Console.WriteLine("Factorial is:{0}", ret);
+        }
+    }
+
+    class Factorial
+    {
+        public int Ops(int No)
+        {
+            if (No <= 0)
+            {
+                return -1;
+            }
+
+            int fact = 1;
+
+            for (int i = 1; i <= No; i++)
+            {
+                fact = fact * i;
+            }
+
+            return fact;
+        }
+    }
+}
