71c155f459445cc8d1ab60b3916e6cf2bfb5865e
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 10:39:17 +0530

    Add files via upload

---
+++ Angular/Typescript/Variables Declarations/3_VariablesDeclrations.js
@@ -0,0 +1,12 @@
+/*
+Application which demonstate diffrent ways in which we can create variable.
+*/
+// Variable declared with data type and initialization
+var name1 = "Marvellous Infosystems";
+// Variable declared with data type only.
+var name2;
+// Variable declared without data type. 
+//Data type is predicted upon the initialization
+var name3 = "Marvellous Infosystems";
+// Data type is considered as any
+var name4;
---
+++ Angular/Typescript/Variables Declarations/3_VariablesDeclrations.ts
@@ -0,0 +1,16 @@
+/*
+Application which demonstate diffrent ways in which we can create variable.
+*/
+
+// Variable declared with data type and initialization
+var name1:string = "Marvellous Infosystems";
+
+// Variable declared with data type only.
+var name2:string;
+
+// Variable declared without data type. 
+//Data type is predicted upon the initialization
+var name3 = "Marvellous Infosystems";
+
+// Data type is considered as any
+var name4;
