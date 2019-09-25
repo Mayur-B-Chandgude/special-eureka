168152491a95903d28c1a76266873d37c2d2fe1b
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-25 09:06:12 +0530

    Add files via upload

---
+++ C#/Assignments/Assignment Number 9/Problem Statement 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Problem_Statement_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marvellous mobj = new Marvellous();
+
+            Thread t1 = new Thread(new ThreadStart(mobj.Display));
+
+            t1.Start();
+        }
+    }
+
+    public class Marvellous
+    {
+        public void Display()
+        {
+            Thread t = Thread.CurrentThread;
+
+            t.Name = "Marvellous";
+
+            Console.WriteLine("Thread Name is:{0}",t.Name);
+            Console.WriteLine("Thread Priority is:{0}",t.Priority);
+            Console.WriteLine("Thread ID is:{0}",t.ManagedThreadId);
+
+            if (t.IsAlive)
+            {
+                Console.WriteLine("Thread is Alive.");
+            }
+
+            Console.WriteLine("Thread State is:{0}",t.ThreadState);
+        }
+    }
+}
