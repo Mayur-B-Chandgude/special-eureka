35afa43c791513e9d53b0dcef0a97da4e01344ed
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-27 13:44:50 +0530

    Add files via upload

---
+++ Angular/Concepts/Routing/Routing-Navigation/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BatchesComponent } from './batches/batches.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+
+
+const routes: Routes = [
+  {path : 'batches', component:BatchesComponent},
+  {path : 'subjects', component:SubjectsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
