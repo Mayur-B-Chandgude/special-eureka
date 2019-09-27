13c2b9f09e5c8b2cf59252abed9d003abdff9270
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-27 08:50:08 +0530

    Add files via upload

---
+++ Angular/Concepts/Forms Applications/Admission Form Without Bootstrap/contact-details/contact-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-details',
+  templateUrl: './contact-details.component.html',
+  styleUrls: ['./contact-details.component.css']
+})
+export class ContactDetailsComponent
+{
+  public batches=['Pre-Placement Activity','Logic Building','Angular','Python','Industrial Project Development']
+}
