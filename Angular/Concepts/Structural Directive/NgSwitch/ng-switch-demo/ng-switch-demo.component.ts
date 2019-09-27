74e33ec938c5be0fea6b25d743b5209b2387aaf4
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-27 14:16:23 +0530

    Add files via upload

---
+++ Angular/Concepts/Structural Directive/NgSwitch/ng-switch-demo/ng-switch-demo.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch-demo',
+  templateUrl: './ng-switch-demo.component.html',
+  styleUrls: ['./ng-switch-demo.component.css']
+})
+export class NgSwitchDemoComponent implements OnInit 
+{
+
+  constructor() 
+  {
+
+  }
+
+  public batch = "PPA";
+  
+  ngOnInit() {
+  }
+
+}
