5fa92715d7658a552bf88754c941b3468ae5aae3
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-27 09:44:23 +0530

    Add files via upload

---
+++ Angular/Concepts/Custom Component/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
