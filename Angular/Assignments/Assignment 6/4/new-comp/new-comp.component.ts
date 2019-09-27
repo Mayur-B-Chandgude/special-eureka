688b3c749337e8cfc103046b9fa64d17e38f9821
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-27 20:22:56 +0530

    Add files via upload

---
+++ Angular/Assignments/Assignment 6/4/new-comp/new-comp.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  templateUrl: './new-comp.component.html',
+  styleUrls: ['./new-comp.component.css']
+})
+export class NewCompComponent implements OnInit 
+{
+  Name = "Marvellous";
+
+  constructor() 
+  { 
+
+  }
+
+  ngOnInit() 
+  {
+    
+  }
+
+}
