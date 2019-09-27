275fa6f3e0dfaf149271305f4d2909706f41d1b5
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-27 14:10:47 +0530

    Add files via upload

---
+++ Angular/Concepts/Structural Directive/NgIf/ng-if-demo/ng-if-demo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-if-demo',
+  templateUrl: './ng-if-demo.component.html',
+  styleUrls: ['./ng-if-demo.component.css']
+})
+export class NgIfDemoComponent implements OnInit 
+{
+    public flag = true;
+
+  ngOnInit() 
+  {
+
+  }
+
+}
