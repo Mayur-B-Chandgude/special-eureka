639a540cf68933e0e0496827a659950c6cafe95c
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-09-28 08:58:17 +0530

    Add files via upload

---
+++ Angular/Assignments/Assignment 10/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit 
+{
+  
+  constructor() 
+  { 
+
+  }
+
+  ngOnInit() 
+  {
+
+  }
+
+}
