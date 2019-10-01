86215ff67cacf33450e9c9ca13607a5bfa95d26b
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-10-01 09:06:31 +0530

    Add files via upload

---
+++ Angular/Concepts/ViewChild & ViewChildren/View-Children/child/child.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent 
+{
+  today:Date=new Date();
+}
