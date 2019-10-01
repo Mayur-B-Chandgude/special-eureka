efba6055f1c840e6576c33e8a6058ac305e8e4d9
Mayur B Chandgude <53011846+Mayur-B-Chandgude@users.noreply.github.com>
2019-10-01 08:45:19 +0530

    Add files via upload

---
+++ Angular/Concepts/Lazy Loading/admin-module/admin-tasks/admin-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tasks',
+  templateUrl: './admin-tasks.component.html',
+  styleUrls: ['./admin-tasks.component.css']
+})
+export class AdminTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
